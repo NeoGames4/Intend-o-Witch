938e81a79c69790b0610ea641ce2acd59ac9b39d
1Dr490n <idragon3o66@gmail.com>
2025-01-03 04:22:55 +0100

    Refactored inputs

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-import RPi.GPIO as GPIO
-from signal import signal, SIGTERM, SIGHUP, pause
-from smbus import SMBus
-from gpiozero import PWMLED
 from time import sleep
 import pygame
 from graphics import draw
-
-bus = SMBus(1)
-ads7830_commands = (0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4)
-
-def safe_exit(signum, frame):
-    pygame.quit()
-    exit(1)
-
-def read_ads7830(input):
-    bus.write_byte(0x4b, ads7830_commands[input])
-    return bus.read_byte(0x4b)
+from game import *
+import input
 
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((480, 320))
 
+input.Button(17, lambda: print("Punch"))
+
 try:
-    signal(SIGTERM, safe_exit)
-    signal(SIGHUP, safe_exit)
+    input.init()
     
     while True:
+        clock.tick(60)
         
+        vx = input.read_x()
+        vy = input.read_y()
         
-        screen.fill((255, 0, 0))
+        player.x += (vx - 128) // 4 / 32
+        player.y += (vy - 128) // 4 / 32
         
+        input.handle_buttons()
         
         draw(screen)
-        
-        
-        pygame.display.flip()
     
 except KeyboardInterrupt:
     pass
